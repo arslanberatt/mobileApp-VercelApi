20c7dd473313d7fdc3d7cdbd10a8766babe3dd5d
Artyom <162967923+arslanberatt@users.noreply.github.com>
2025-03-25 16:54:06 +0300

    Update route.ts

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -6,25 +6,13 @@ import { prismadb } from "@/lib/db";
 const SECRET_KEY = process.env.JWT_SECRET || "default_secret";
 
 export async function POST(req: Request) {
-  const headers = {
-    "Access-Control-Allow-Origin": "*",  // Bu satır tüm domainlere izin verir
-    "Access-Control-Allow-Methods": "POST, OPTIONS", // Hangi HTTP metodlarına izin verileceğini belirtir
-    "Access-Control-Allow-Headers": "Content-Type, Authorization", // Hangi header'ların izinli olduğunu belirtir
-  };
-
-  // CORS OPTIONS isteği için cevap ver
-  if (req.method === "OPTIONS") {
-    return NextResponse.json({}, { status: 200, headers });
-  }
-
-  // Asıl API işlemleri burada
   try {
     const { email, password } = await req.json();
 
     if (!email || !password) {
       return NextResponse.json(
         { message: "Email ve şifre zorunludur!" },
-        { status: 400, headers }
+        { status: 400 }
       );
     }
 
@@ -33,7 +21,7 @@ export async function POST(req: Request) {
     if (!user || user.role !== "admin") {
       return NextResponse.json(
         { message: "Geçersiz giriş bilgileri veya admin değil!" },
-        { status: 401, headers }
+        { status: 401 }
       );
     }
 
@@ -42,7 +30,7 @@ export async function POST(req: Request) {
     if (!isPasswordValid) {
       return NextResponse.json(
         { message: "Geçersiz giriş bilgileri!" },
-        { status: 401, headers }
+        { status: 401 }
       );
     }
 
@@ -52,17 +40,16 @@ export async function POST(req: Request) {
 
     return NextResponse.json(
       { message: "Giriş başarılı!", token },
-      { status: 200, headers }
+      { status: 200 }
     );
   } catch (error) {
     console.error("Backend error:", error);
     return NextResponse.json(
       { message: "Bir şeyler ters gitti, lütfen tekrar deneyin." },
-      { status: 500, headers }
+      { status: 500 }
     );
   }
 }
-
 export async function GET() {
   try {
     const hotels = await prismadb.user.findMany();
